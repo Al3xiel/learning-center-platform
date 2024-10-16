af349c832f1f9f008fa2dc3193d49ed772371902
Al3xiel <gabriel280104casas@gmail.com>
2024-10-16 17:04:05 -0500

    feat(profiles): addded profile queries handle to aggregate

---
+++ src/main/java/pe/upc/learningcenterplatform/profiles/domain/services/ProfileQueryService.java
@@ -0,0 +1,16 @@
+package pe.upc.learningcenterplatform.profiles.domain.services;
+
+import pe.upc.learningcenterplatform.profiles.domain.model.aggregates.Profile;
+import pe.upc.learningcenterplatform.profiles.domain.model.queries.GetAllProfilesQuery;
+import pe.upc.learningcenterplatform.profiles.domain.model.queries.GetProfileByEmailQuery;
+import pe.upc.learningcenterplatform.profiles.domain.model.queries.GetProfileByIdQuery;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+public interface ProfileQueryService {
+    Optional<Profile> handle(GetProfileByIdQuery query);
+    Optional<Profile> handle(GetProfileByEmailQuery query);
+    List<Profile> handle(GetAllProfilesQuery query);
+}
